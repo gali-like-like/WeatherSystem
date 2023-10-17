a8be5f4241bbfdc3e4497e3d9b95b425f0d5688a
tom <123@123.com>
2023-10-17 18:10:11 +0800

    main.cpp添加一行打印456

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     qDebug()<<"123";
+    qDebug()<<"456";
     QTranslator translator;
     const QStringList uiLanguages = QLocale::system().uiLanguages();
     for (const QString &locale : uiLanguages) {
